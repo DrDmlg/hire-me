1d1d9f08ec0c3d25629692987c5e7b3c317f4140
Dmitry Dorokhov <b27011971@inbox.ru>
2026-01-08 22:01:41 +0300

    feat: реализована возможность менять статус поиска работы в профайле кандидата

--- js/manager/CandidateProfileManager.js
+++ js/manager/CandidateProfileManager.js
@@ -2,6 +2,7 @@ class CandidateProfileManager {
 
     constructor(profileData) {
         this.tg = window.Telegram?.WebApp;
+        this.api = apiService;
         this.profileData = profileData; // Пока данными мы никак не оперируем, но они в дальнейшем понадобятся
         this.candidateProfile = this.getTemporaryCandidateProfileData(); // временные тестовые данные
     }
@@ -33,7 +34,7 @@ class CandidateProfileManager {
     getTemporaryCandidateProfileData() {
         return {
             position: this.profileData.candidate.desiredPosition,
-            isActive: true, // TODO: пока мок значение. Исправить реализацю как дойдут руки
+            jobStatus: this.profileData.candidate.candidateJobStatus,
             stats: {
                 profileViews: 0,
                 responses: 0,
@@ -54,7 +55,7 @@ class CandidateProfileManager {
         // Обработчик для статуса
         const statusTag = document.getElementById('statusTag');
         if (statusTag) {
-            statusTag.addEventListener('click', () => this.toggleStatus());
+            statusTag.addEventListener('click', () => this.changeCandidateJobStatus());
         }
     }
 
@@ -73,13 +74,6 @@ class CandidateProfileManager {
         }
     }
 
-    toggleStatus() {
-        this.candidateProfile.isActive = !this.candidateProfile.isActive;
-        this.setCandidateJobStatus();
-
-        const statusText = this.candidateProfile.isActive ? 'Активный поиск' : 'Не ищу работу';
-    }
-
     updateCandidateProfileData() {
         this.setUserAvatar();
         this.setUserName();
@@ -96,19 +90,39 @@ class CandidateProfileManager {
         UserProfileFiller.setUserName(this.profileData);
     }
 
+    async changeCandidateJobStatus() {
+        const candidateId = this.profileData?.candidate?.id;
+
+            const currentStatus = this.candidateProfile.jobStatus;
+            const newStatus = currentStatus === 'Активный поиск' ? 'Не ищу работу' : 'Активный поиск';
+
+            try {
+                const response = await this.api.put(`/candidate/status/${candidateId}`, {
+                    jobStatus: newStatus
+                });
+
+                if (response.status >= 200 && response.status < 300) {
+                    this.candidateProfile.jobStatus = newStatus;
+                    this.setCandidateJobStatus();
+                }
+            } catch (error) {
+                notification.error('Ошибка смены статуса');
+            }
+        }
+
     setCandidateJobStatus() {
         const statusElement = document.getElementById('userStatus');
         const statusDot = document.getElementById('statusDot');
 
         if (!statusElement || !statusDot) return;
 
-        if (this.candidateProfile.isActive) {
+        const isActive = this.candidateProfile.jobStatus === 'Активный поиск';
+
+        if (isActive) {
             statusElement.textContent = 'Активный поиск';
-            statusDot.className = 'status-dot';
             statusDot.style.background = '#10E6A0';
         } else {
             statusElement.textContent = 'Не ищу работу';
-            statusDot.className = 'status-dot inactive';
             statusDot.style.background = '#e21212';
         }
     }
